4bffac561553c7974f0a50342775a926ec0c0cc6
llum602 <32022929+llum602@users.noreply.github.com>
2017-11-02 16:38:07 -0400

    Added navigation

--- src/Create.js
+++ src/Create.js
@@ -4,7 +4,6 @@ import './App.css';
 const Create = () => (
   <div className="Create">
   <body styles="background-color:#32516E;">
-    
     <div className= "block"></div>
       <p align = "right"><a className = "btn btn-lg btn-primary" href = "index.html">Export</a></p>
   </body>
