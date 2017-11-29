100fdb916b304512e15c894ebabab5a69e497726
llum602 <32022929+llum602@users.noreply.github.com>
2017-11-29 17:05:38 -0500

    Added logo

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import mainLogo from'./chordIcon2.png';
 import './App.css';
 
 const Header = () => (
     <header className="page-header">
         <ul align = "middle">
-            <h3>
-                <li><img src="chordIcon2.png" alt="chord logo"/></li>
+            <h4>
+            <img src={mainLogo} className={"logo"} alt="Logo"/>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
                 <li><Link to='/'>Home</Link></li>
                 <li><Link to='/create'>Create</Link></li>
-            </h3>
+            </h4>
         </ul>
     </header> 
 )
